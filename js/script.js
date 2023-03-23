0386242a31a7c46cc91767570fdcda131c2f634a
charlotte-jhu <124389290+charlotte-jhu@users.noreply.github.com>
2023-03-23 17:12:09 +0000

    added inital code for website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Charlote Jhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Charlotte Jhu
+// Created on: March 2023
 // This file contains the JS functions for index.html
+
+'use strict'
+/**
+ * This function calculates the circumference of a circle
+ */
+function myButtonClicked() {
+  // input
+  const radius = parseInt(document.getElementById("radius").value)
+
+  // process
+  const circumference = 2 * 3.14 * radius
+
+  // output
+  document.getElementById("circumference").innerHTML = "The circumference is " + circumference + "cm"
+}
